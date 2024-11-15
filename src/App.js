b6f933e910d96b9fe884e65cad439e68ab6dc024
RagaviRajasekaran <143269843+RajRagavi@users.noreply.github.com>
2024-11-15 20:04:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+// import IndexPage from './components/IndexPage';
+import SendBulkEmail from './components/SendBulkEmail';
+// import Dashboard from './components/Dashboard';
+// import LoginPage from './components/LoginForm';
+// import RegisterPage from './components/RegisterForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+       
+       
+
+        {/* Main content area */}
+        <div style={{  width: '100%' }}>
+          <Routes>
+            {/* Default route to Index Page */}
+            <Route path="/" element={<SendBulkEmail />} />
+
+            {/* Protected Routes */}
+            <Route path="/send-bulk-email" element={<SendBulkEmail />} />
+            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+            {/* <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} /> */}
+
+            {/* Redirect to home if route not found */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
