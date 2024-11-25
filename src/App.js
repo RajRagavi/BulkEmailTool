369b1a43a8187a3fb14ffd099d018a04832b33ed
RagaviRajasekaran <143269843+RajRagavi@users.noreply.github.com>
2024-11-25 20:17:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-// import IndexPage from './components/IndexPage';
+import IndexPage from './components/IndexPage';
 import SendBulkEmail from './components/SendBulkEmail';
-// import Dashboard from './components/Dashboard';
-// import LoginPage from './components/LoginForm';
-// import RegisterPage from './components/RegisterForm';
+import Dashboard from './components/Dashboard';
+import LoginPage from './components/LoginForm';
+import RegisterPage from './components/RegisterForm';
 
 function App() {
   return (
@@ -19,13 +18,13 @@ function App() {
         <div style={{  width: '100%' }}>
           <Routes>
             {/* Default route to Index Page */}
-            <Route path="/" element={<SendBulkEmail />} />
+            <Route path="/" element={<IndexPage />} />
 
             {/* Protected Routes */}
             <Route path="/send-bulk-email" element={<SendBulkEmail />} />
-            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-            {/* <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} /> */}
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
 
             {/* Redirect to home if route not found */}
             <Route path="*" element={<Navigate to="/" />} />
